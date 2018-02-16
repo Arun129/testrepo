d9660c7b49641af911e84672914ed676344b925f
Ark <arunkarthik29890@gmail.com>
2018-02-16 16:54:18 +0530

    test change 1
    test change 1

--- handler.js
+++ handler.js
@@ -11,7 +11,7 @@ module.exports.hello = (event, context, callback) => {
   callback(null, response);
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+  // callback(null, { message: 'Go Serverlessss v1.0! Your function executed successfully!', event });
 };
 
 module.exports.imageResize = (event, context, callback) => {
