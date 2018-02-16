16a6202570e01c649d677623525de31fcc5d6ba6
Ark <arunkarthik29890@gmail.com>
2018-02-16 23:43:02 +0530

    testcommit4
    testcommit4

--- handler.js
+++ handler.js
@@ -1,30 +1,4 @@
-'use strict';
-
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Hello!',
-    }),
-  };
-
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverlessss v1.0! Your function executed successfully!', event });
-};
-
-module.exports.imageResize = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Image Resizer!',
-
-    }),
-  };
-
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+exports.handler = (event, context, callback) => {
+    // TODO implement
+    callback(null, 'Hello from Lambda');
 };
