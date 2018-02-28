fd3aa1cf40298e7156b9d08e2c013f650c93d7e1
Ark <arunkarthik29890@gmail.com>
2018-02-28 12:54:29 +0530

    democommit2
    democommit2

--- handler.js
+++ handler.js
@@ -4,7 +4,7 @@ module.exports.hello = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Hello World - demo edit by arun!',
+      message: 'Hello World!',
     }),
   };
 
